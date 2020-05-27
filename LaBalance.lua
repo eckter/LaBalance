621035c71e597b8baecb66c174e11198b42e3566
Florian Perlié-Long <fpl.vlp77@gmail.com>
2020-05-28 00:04:16 +0200

    on va reprendre cet addon miteux

--- LaBalance.lua
+++ LaBalance.lua
@@ -7,14 +7,38 @@ local raid_pds_dict = {}
 local cur_num
 local cmd_name = "/labalance"
 
+
+ -- list of interesting events which could be used later
+ -- ZONE_CHANGED_NEW_AREA
+ -- Group_roster_update
+
+frame:RegisterEvent("ENCOUNTER_START")
+frame:RegisterEvent("ENCOUNTER_END")
 frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-frame:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
-frame:RegisterEvent("PLAYER_REGEN_ENABLED")
 frame:RegisterEvent("ADDON_LOADED")
 
 frame:SetScript("OnEvent", function(self, event_name, ...)
     return self[event_name](self, event_name, ...)
 end)
+
+local avoidable = {
+
+    -- shadar hm ?
+    [306928] = 42, -- souffle ombreux
+    [306929] = 42, -- souffle entropique
+    [306930] = 42, -- souffle bouillonant
+    -- irion MM
+    [307053] = 42, -- flaque de lave
+    [306735] = 42, -- cataclysme
+                  -- bourrasque explosive
+                  -- claque-queue
+                  -- folie rampante
+    -- Skitra MM
+                  -- images deferlantes
+    -- maut MM
+    [314337] = 42, -- malediction
+    [308044] = 42 -- annihilation
+}
 
 
 function trash(player_name)
@@ -24,14 +48,10 @@ function trash(player_name)
         player_name .. " feint la mort avec un réalisme a couper le souffle !",
         player_name .. " préfère discuter avec Bwonsamdi.",
         player_name .. " n'a pas réussi à maintenir ses points de vie au dessus de zéro.",
-        "Qu'est-ce que c'est que ce truc sous mes semelles ? Ah, c'est " .. player_name " !"
+        "Qu'est-ce que c'est que ce truc sous mes semelles ? Ah, c'est " .. player_name .. " !",
+        player_name .. ", heure du décès : " .. date("%H:%M.")
     }
-
-    local reminder = { "Et la pierre de soin alors ?", "Pourtant, il y a une pierre de soin dans ses sacs.",
-        "La pierre de soin aurait pu l'aider."
-    }
-
-    send_message(punchlines[random(table.getn(punchlines))] .. " " .. reminder[random(table.getn(reminder))])
+    send_message(punchlines[random(table.getn(punchlines))])
 end
 
 
@@ -54,12 +74,13 @@ end
 function command_chan(msg)
     cmd, args = strsplit(" ", msg, 2)
     if cmd == "set" then
-        if not args:match("^[a-zA-Z]+$") then
+        if not args:match("^[0-9a-zA-Z]+$") then
             print("error: invalid chan name")
             return false
         end
         la_balance_save["chan"] = args
         print(addon_name .. ": set output to chan " .. args)
+        send_message("Test.")
     elseif cmd == "disable" then
         la_balance_save["chan"] = nil
     else
@@ -94,29 +115,23 @@ function frame:ADDON_LOADED(event_name, name)
     end
 end
 
-function frame:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
-    if IsEncounterInProgress() and not in_combat then  -- returns 1 when you're in a fight you can't release from
-    	in_combat = true
-    	print("engaging combat")
-        cur_num = GetNumGroupMembers()
-        for i = 1, cur_num do
-            name, rank, subgroup, level, class, fileName, zone, online, isDead,
-            	role, isML = GetRaidRosterInfo(i);
-            raid_name_dict[i] = name
-            raid_pds_dict[name] = false
-        end
+function frame:ENCOUNTER_START(encounterID, encName, difficulty, size)
+    in_combat = true
+    print("engaging combat with " .. encName .. ".")
+    cur_num = GetNumGroupMembers()
+    for i = 1, cur_num do
+        name, rank, subgroup, level, class, fileName, zone, online, isDead,
+            role, isML = GetRaidRosterInfo(i);
+        raid_name_dict[i] = name
     end
 end
 
-function frame:PLAYER_REGEN_ENABLED()
-    if in_combat then
-        in_combat = false
-        print("end of combat")
-        --for key,value in pairs(raid_pds_dict) do send_message(key .. (value and " a " or " n'a pas ")
-          --  .. "utilisé sa pierre de soin") end
-        raid_name_dict = {}
-        raid_pds_dict = {}
-    end
+function frame:ENCOUNTER_END(encounterID, name, difficulty, size, success)
+    in_combat = false
+    print("end of combat")
+    --for key,value in pairs(raid_pds_dict) do send_message(key .. (value and " a " or " n'a pas ")
+    --  .. "utilisé sa pierre de soin") end
+    raid_name_dict = {}
 end
 
 function frame:COMBAT_LOG_EVENT_UNFILTERED(event,...)
@@ -129,20 +144,26 @@ function frame:COMBAT_LOG_EVENT_UNFILTERED(event,...)
         local type, zero, server_id, instance_id, zone_uid, npc_id, spawn_uid = strsplit("-", dstGUID);
         if type == "Player" then
             print(dstName .. " est mort ! BOUH !")
-            if in_combat and not raid_pds_dict[dstName] then
+            if in_combat then
                 trash(dstName)
             end
         end
     end
 
+
+
     if prefix:match("^SPELL") or prefix == "RANGE" then
-        local spellId, spellName, spellSchool = select(12, unpack(event))
-        if spellId == 6262 and suffix:match("HEAL$") then
-            send_message(srcName .. " a utilisé une pierre de soins ! GG !")
-            if in_combat then
-                raid_pds_dict[srcName] = true
+        if suffix == "DAMAGE" then
+            local spellId, spellName, spellSchool, amount, overkill,
+                school, resisted, blocked, absorbed, critical, glancing, crushing = select(12, unpack(event))
+            if avoidable[spellId] then
+                send_message(dstName .. " a pris " .. amount .. " de " .. GetSpellLink(spellId))
+            end
+        else
+            local spellId, spellName, spellSchool = select(12, unpack(event))
+            if spellId == 6262 and suffix:match("HEAL$") then
+                print(srcName .. " a utilisé une pierre de soins ! GG !")
             end
         end
-        skip = 3
     end
 end
